e07ae20a22d03cdf4a62cd2465ef37f38856defe
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-10 19:00:02 -0400

    Create test for summary form

---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import SummaryFrom from '../SummaryForm';
+
+test('should not have checkbox checked initially', () => {
+    render(<SummaryForm/>);
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).not.toBeChecked;    
+})
+
+test('should enable button on check, and disable on second check', () => {
+    render(<SummaryForm/>);
+
+    const checkbox = screen.getByRole('checkbox');
+
+    fireEvent.click(checkbox);
+
+    const button = screen.getByRole('button', {name:"Confirm Order"})
+
+    expect(button).toBeEnabled;
+
+    fireEvent.click(checkbox);
+
+    expect(checkbox).not.toBeChecked;
+    expect(button).toBeDisabled;
+})
