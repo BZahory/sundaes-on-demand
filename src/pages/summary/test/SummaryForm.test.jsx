57ff7fba177a49e1874a6498855e855993d35715
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-10 21:14:56 -0400

    Update and test SummaryForm

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {render, fireEvent, waitFor, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
+import {render, waitFor, screen, waitForElementToBeRemoved} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import SummaryForm from '../SummaryForm';
+import {createRoot} from 'react-dom/client'
 
 test('should not have checkbox checked initially', () => {
     render(<SummaryForm/>);
@@ -14,14 +16,33 @@ test('should enable button on check, and disable on second check', () => {
 
     const checkbox = screen.getByRole('checkbox');
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
 
     const button = screen.getByRole('button', {name:"Confirm order"})
 
     expect(button).toBeEnabled;
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
 
     expect(checkbox).not.toBeChecked;
     expect(button).toBeDisabled;
+})
+
+test('should make tooltip visible on hover', async () => {
+    render(<SummaryForm/>);
+    
+    const nullPopever = screen.queryByText(/No ice cream will actually be delivered/i);
+
+    expect(nullPopever).not.toBeInTheDocument();
+
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditions);
+
+    const getByPopover = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(getByPopover).toBeInTheDocument();
+
+    userEvent.unhover(termsAndConditions);
+    await waitForElementToBeRemoved(() => screen.queryByText(/No ice cream will actually be delivered/i));
+
+    
 })
