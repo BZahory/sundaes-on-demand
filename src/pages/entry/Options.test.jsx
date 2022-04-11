17fe4b8ffa09b50403275dff40acf1850f79a5f8
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-11 18:03:36 -0400

    Add tests for toppings

--- src/pages/entry/Options.test.jsx
+++ src/pages/entry/Options.test.jsx
@@ -8,9 +8,21 @@ test('should display image for each scoop option from server', async () => {
     const scoopImages = await screen.findAllByRole('img', {name: /scoop$/i});
     expect(scoopImages).toHaveLength(2);
 
-    const altText = scoopImages.map((element)=>element.alt);
+    const altText = scoopImages.map((e)=>e.alt);
 
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 
+    
+})
+
+test('should display image for each topping option from server', async () => {
+    render(<Options optionType="toppings"/>);
+
+    const toppingImages = await screen.findAllByRole('img', {name: /topping$/i});
+    expect(toppingImages).toHaveLength(3);
+
+    const altText = toppingImages.map((e)=>e.alt);
+
+    expect(altText).toEqual(['Cherries', 'M&Ms', 'Hot fudge'].map((e) => e + " topping"));
     
 })
